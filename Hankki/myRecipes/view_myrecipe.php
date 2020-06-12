6b7007081b79f447077b4702a70ec223a2badaca
jin-you <58108092+jin-you@users.noreply.github.com>
2020-06-12 18:45:24 +0900

    Update view_myrecipe.php
    view_myrecipe 최종

--- Hankki/myRecipes/view_myrecipe.php
+++ Hankki/myRecipes/view_myrecipe.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
-$link = mysqli_connect("localhost", "whalsrl5650", "whalsrl5650!", "whalsrl5650");
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+	$link = mysqli_connect("localhost", "whalsrl5650", "whalsrl5650!", "whalsrl5650");
+	if($link === false){
+	    die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
 
 	$id = $_REQUEST["recipeID"];
 	$sql = "select * from whalsrl5650.Recipe where recipeID=$id";
@@ -11,10 +11,9 @@ if($link === false){
 	$result = mysqli_query($link, $sql);
 
 	$row=mysqli_fetch_assoc($result);
-	$count=$row['cookTime'];
 
 	$item_nick = $row['nickname'];
-  $item_id = $row['userID'];
+  	$item_id = $row['userID'];
 
 	$item_title = str_replace(" ", "&nbsp;", $row['title']);
 
@@ -36,29 +35,17 @@ if($link === false){
 	$sql1 = "update whalsrl5650.Recipe set view_count=$new_hit where recipeID=$id;"; // 글 조회수 증가
 	$result1=mysqli_query($link, $sql1);
 
-	 $sql2="select * from whalsrl5650.Step where recipeID=$id";
-	 $result2=mysqli_query($link, $sql2);
+	$sql2="select * from whalsrl5650.Step where recipeID=$id";
+	$result2=mysqli_query($link, $sql2);
 
-	 $row_step=mysqli_fetch_assoc($result2);
-	 $count=0;
-
-	$step1 = $row_step['step1'];
-	if(!empty($step1)){
-		$count++;
-	}
-
-	$step2 = $row_step['step2'];
-	if(!empty($step2)){
-		$count++;
-	}
-
-	$step3 = $row_step['step3'];
-	if(!empty($step3)){
-		$count++;
-	}
+	$row_step=mysqli_fetch_assoc($result2);
 	
+	$count=$row_step['step_count'];
 
-
+	$sql_time="select * from whalsrl5650.Timer where recipeID=$id";
+	$result_time=mysqli_query($link, $sql_time);
+	
+	$row_time=mysqli_fetch_assoc($result_time);
 ?>
 
 
@@ -104,72 +91,61 @@ if($link === false){
 			<h1 class="text-center"><?php echo $item_title?></h1>
 			<p class="text-center"></p>
 		</div>
-    </div>
+	</div>
 	</br>
 
 
-
-    <div id="quicknav">
-        <ul>
-			<?php
-			for($step_count=1; $step_count <= $count; $step_count++){?>
-				<li><a href="#<?php echo $step_count?>" class="btn btn-primary">step <?php echo $step_count?> </a></li>
-
-			<?php } ?>
-        </ul>
-    </div>
+<div id="quicknav">
+	<ul>
+	<?php	for($step_count=1; $step_count <= $count; $step_count++){ ?>
+		<li><a href="#<?php echo $step_count?>" class="btn btn-primary">step <?php echo $step_count?> </a></li>
+	<?php } ?>		
+	</ul>
+</div>
+	
 	<br><br>
-
-	<div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default coupon">
-              <div class="panel-heading" id="head">
-			  
+	
+<div class="row">
+	<div class="col-md-6 col-md-offset-3">
+		<div class="panel panel-default coupon"> 
+		<div class="panel-heading" id="head">	  
                 <div class="panel-title" id="title">
-					
-                    <span class="hidden-xs"><?php echo $item_title?></span>
-                    <span class="visible-xs"><?php echo $item_title?></span>
+			<span class="hidden-xs"><?php echo $item_title?></span>
+                    	<span class="visible-xs"><?php echo $item_title?></span>
                 </div>
-              </div>
-              <div class="panel-body">
-				<?php echo "<img src='$item_image' class='coupon-img img-rounded'>";?>
-				<div class="col-md-9">
-                    <ul class="items">
-						<li>가격 : <?php
-						if ($item_price==5000){ echo '만원 미만';}
-						else if ($item_price ==15000) {echo '만원~ 2만원';}
-						else if ($item_price == 25000) {echo '2만원 ~ 3만원';}
-						else if ($item_price == 50000) {echo '3만원 이상';}
-						?> </li>
-                        <li>재료 : <?php echo $item_ingredients ?></li>
-                    </ul>
+              	</div>
+              	<div class="panel-body">
+			<?php echo "<img src='$item_image' class='coupon-img img-rounded'>";?>
+			<div class="col-md-9">
+                    	<ul class="items">
+				<li>가격 : <?php echo $item_price; ?> </li>
+                        	<li>재료 : <?php echo $item_ingredients ?></li>
+                    	</ul>
                 </div>
-
+			
                 <div class="col-md-12">
-                    <p class="disclosure">부가설명 : <?php echo $item_content ?></p>
+                    	<p class="disclosure">부가설명 : <?php echo $item_content ?></p>
                 </div>
-              </div>
-              <div class="panel-footer">
-                <div class="coupon-code">
-                    작성일 : <?php echo $item_date?>
-                </div>
-                <div>
-			작성자 : <?php echo $item_nick?>
-		</div>
-		
-              </div>
+              	</div>
+              
+		<div class="panel-footer">
+                	<div class="coupon-code">
+                	    작성일 : <?php echo $item_date?>
+                	</div>
+                	<div>
+				작성자 : <?php echo $item_nick?>
+			</div>
+              	</div>
 
             </div>
     	</div>
     </div>
-
 	<?php
-
 	for($start_count=1; $start_count<=$count; $start_count++){
 		$word="step";
 		$now_step=$word.$start_count;
 	?>
-
+	
 	<div class="row" id="<?php echo $start_count?>">
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default coupon">
@@ -178,53 +154,41 @@ if($link === false){
                     <span class="hidden-xs">step <?php echo $start_count?></span>
                     <span class="visible-xs">step <?php echo $start_count?></span>
                 </div>
-              </div>
-              <div class="panel-body">
-                <div class="col-md-9">
+             </div>
+              
+	<div class="panel-body">
+		<div class="col-md-9">
                     <ul class="items">
-                        <li><?php echo $$now_step ?></li>
+                        <li><?php echo $row_step[$now_step];?></li>
                     </ul>
                 </div>
-              </div>
-
-            </div>
-    	</div>
+				
+		<br><br><br>
+		<?php if(empty($row_time[$now_step])==false){?>
+			<div align="center">
+			<span id=<?php echo $now_step?>>
+			<h1><b> <?php echo floor($row_time[$now_step]/60)?>분&nbsp<?php echo ($row_time[$now_step]%60)?>초</b></h1> 
+			</span>	
+			<br>
+				
+			<div class="wrap"><button class="button" onclick="timer(<?php echo $row_time[$now_step]?>, <?php echo "'".$now_step."'"?>)">시작</button></div>
+			<br><br>		
+			</div>
+			<?php } ?>
+				
+              		</div>
+	</div>
     </div>
-	<?php } ?>
-
+    </div>
+<?php } ?>
 </div>
 
-
-	<div align="center">
-		<strong><h2>Timer</h2></strong><br>
-		<span id="countdown"></span>
-		<br>
-		<div class="wrap"><button class="button" onclick="timer();">시작</button></div>
-		<br><br>
-
-
-	</div>
-
-<?php
-		$sql3 = "select cookTime from whalsrl5650.Recipe where recipeid=$id;";
-
-		$result3 = mysqli_query($link, $sql3);          
-
-		$row3=mysqli_fetch_assoc($result3);
-		$count=$row3['cookTime'];
-		echo "<script>var count = '".$count."';</script>";
-
-
-?>
-
 <script type="text/javascript">
-    var time = 0;
-    var count_re=count;
-	var count_min=parseInt(count_re/60);
-	var count_sec=count_re%60;
-
-	document.getElementById("countdown").innerHTML="<h1><b>" +count_min+"분&nbsp"+count_sec+"초</b></h1>";
-
+   	var time = 0;
+	var count;
+	var count_re;
+	var pivot;
+	
 	function play() {
 		var audio = document.getElementById('audio_play');
 		if (audio.paused) {
@@ -235,34 +199,31 @@ if($link === false){
 		}
 	}
 
-    function timer() {
-        clearInterval(time);
-        time = setInterval("myTimer()", 1000);
+   function timer(num,str){
+		pivot=str;
+		count=num;
+		count_re=count;
+		var count_min=parseInt(count_re/60);
+		var count_sec=count_re%60;
+        	clearInterval(time);
+        	time = setInterval("myTimer()", 1000);
 
     }
 
     function myTimer() {
-        count_re= count_re - 1;
+        	count_re= count_re - 1;
 		count_min=parseInt(count_re/60);
 		count_sec=count_re%60;
 
-        document.getElementById("countdown").innerHTML = "<h1><b>" + count_min + "분&nbsp" + count_sec + "초</b></h1>";
-        if (count_re == 0) {
-            clearInterval(time); // 시간 초기화
-            count_re=count;
+	    	document.getElementById(pivot).innerHTML = "<h1><b>" + count_min + "분&nbsp" + count_sec + "초</b></h1>";
+         	if (count_re == 0) {
+         		clearInterval(time); // 시간 초기화
+            		count_re=count;
 			count_min=parseInt(count_re/60);
 			count_sec=count_re%60;
 			play();
-        }
+        	}
     }
-
-	function getIndex(ele){
-		var i=0;
-		while((ele=ele.previousSiblint)!=null){
-			i++;
-		}
-		return i;
-	}
 	
 
 	function del_myrecp(){
@@ -271,8 +232,15 @@ if($link === false){
 			type:"GET",
 			data:{'id':<?php echo $id?>},
 			dataType:"json",
+			success:function(output, textstatus){
+				console.log(output);
+				alert(output);
+			},
+			error:function(request,status,error){
+				alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+			}
 		});
-	  return false;
+		history.back();
 	}
 
 </script>
