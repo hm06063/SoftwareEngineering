7da0e648268397059b225121637c8476a40af89b
Minki-Cho <62167266+Minki-Cho@users.noreply.github.com>
2020-06-27 01:15:50 +0900

    Update top_menu_1.php
    white tema topmenu

--- Hankki/lib/top_menu_1.php
+++ Hankki/lib/top_menu_1.php
@@ -11,7 +11,7 @@
     </div>
 
     <!-- Header Section Begin -->
-    <header class="header header--normal">
+    <header class="header header--normal"><!--top menu1 은 상단바가 흰색 -->
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-3 col-md-3">
@@ -24,23 +24,26 @@
                         <nav class="header__menu mobile-menu">
                             <ul>
                                 <li class="active"><a href="../index.php">Home</a></li>
-                                <li><a href="../board/list.php">전체 레시피</a></li>
+                                <li><a href="../list/All_list_view.php">전체 레시피</a></li>
                                 <li><a href="#####">카테고리</a>
                                 <ul class="dropdown">
-                                	<li><a href="../board/Rice.php">밥</a></li>
-					<li><a href="../board/Maindish.php">메인반찬</a></li>
-					<li><a href="../board/Subdish.php">밑반찬</a></li>
-					<li><a href="../board/Soup.php">국</a></li>                                       
-					<li><a href="../board/Noodle.php">면요리</a></li>
-					<li><a href="../board/Fish.php">해물</a></li>
-					<li><a href="../board/Snack.php">분식</a></li>                                       
-					<li><a href="../board/Drink.php">술안주</a></li>
-					<li><a href="../board/Dessert.php">디저트</a></li>
-                                 </ul>
-				 </li>				
+                                   <li><a href="../list/Rice_view.php">밥</a></li>
+                                        <li><a href="../list/Maindish_view.php">메인반찬</a></li>
+                                         <li><a href="../list/Subdish_view.php">밑반찬</a></li>
+                                        <li><a href="../list/Soup_view.php">국</a></li>                                       
+                                        <li><a href="../list/Noodle_view.php">면요리</a></li>
+                                        <li><a href="../list/Fish_view.php">해물</a></li>
+                                         <li><a href="../list/Snack_view.php">분식</a></li>                                       
+                                        <li><a href="../list/Drink_view.php">술안주</a></li>
+                                        <li><a href="../list/Dessert_view.php">디저트</a></li>
+                                    </ul>
+                                </li>
+								
                                  <li><a href="../freeboard/review_list.php">자유게시판</a></li>
-                                 <li><a href="../join/join_form.php">회원가입</a></li>
+                                   <li><a href="../join/join_form.php">회원가입</a></li>
                                  <li><a href="../login/login_form.php">로그인</a></li>
+
+
                             </ul>
                         </nav>
                     </div>
@@ -73,28 +76,28 @@
                         <nav class="header__menu mobile-menu">
                             <ul>
                                 <li class="active"><a href="../index.php">Home</a></li>
-                                <li><a href="../board/list.php">전체 레시피</a></li>
+                                <li><a href="../list/All_list_view.php">전체 레시피</a></li>
                                 <li><a href="#####">카테고리</a>
                                 <ul class="dropdown">
-                                	<li><a href="../board/Rice.php">밥</a></li>
-                                        <li><a href="../board/Maindish.php">메인반찬</a></li>
-                                        <li><a href="../board/Subdish.php">밑반찬</a></li>
-                                        <li><a href="../board/Soup.php">국</a></li>                                       
-                                        <li><a href="../board/Noodle.php">면요리</a></li>
-                                        <li><a href="../board/Fish.php">해물</a></li>
-                                        <li><a href="../board/Snack.php">분식</a></li>                                       
-                                        <li><a href="../board/Drink.php">술안주</a></li>
-                                        <li><a href="../board/Dessert.php">디저트</a></li>
-                                </ul>
+                                     <li><a href="../list/Maindish_view.php">메인반찬</a></li>
+                                         <li><a href="../list/Subdish_view.php">밑반찬</a></li>
+                                        <li><a href="../list/Soup_view.php">국</a></li>                                       
+                                        <li><a href="../list/Noodle_view.php">면요리</a></li>
+                                        <li><a href="../list/Fish_view.php">해물</a></li>
+                                         <li><a href="../list/Snack_view.php">분식</a></li>                                       
+                                        <li><a href="../list/Drink_view.php">술안주</a></li>
+                                        <li><a href="../list/Dessert_view.php">디저트</a></li>
+                                    </ul>
                                 </li>
                                  <li><a href="../freeboard/review_list.php">자유게시판</a></li>
                                  <li><a href="####">마이페이지</a>
-					 <ul class="dropdown">
-						<li><a href="../board/fav_list.php">즐겨찾기</a></li>
-						<li><a href="../board/mylist.php">내가 쓴 레시피</a></li>
-					 </ul></li>
-                             	 <li><a href="../login/logout.php">로그아웃</a></li>
-                             	  <a class="text-light"><?=$_SESSION["NICKNAME"]?> 님 환영합니다 </a>
+								 <ul class="dropdown">
+										<li><a href="../board/fav_list.php">즐겨찾기</a></li>
+										<li><a href="../board/mylist.php">내가 쓴 레시피</a></li>
+										
+								 </ul></li>
+                              <li><a href="../login/logout.php">로그아웃</a></li>
+                               <a class="text-light"><?=$_SESSION["NICKNAME"]?> 님 환영합니다 </a>
 
                             </ul>
                         </nav>
